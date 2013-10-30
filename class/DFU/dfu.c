bfb1257283cdeffd80c7ad4587736f64a16e69f5
James Snyder <jbsnyder@fanplastic.org>
2013-10-29 19:46:46 -0500

    flash_write and flash_erase functions from SiLabs

--- class/DFU/dfu.c
+++ class/DFU/dfu.c
@@ -56,6 +56,15 @@
  * THE SOFTWARE.
  * ****************************************************************************/
 
+//------------------------------------------------------------------------------
+// Copyright (c) 2012 by Silicon Laboratories. 
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Silicon Laboratories End User 
+// License Agreement which accompanies this distribution, and is available at
+// http://developer.silabs.com/legal/version/v10/License_Agreement_v10.htm
+// Original content and implementation provided by Silicon Laboratories.
+//------------------------------------------------------------------------------
+
 /*!
     \defgroup dfu_class USB DFU Class
     \file dfu.c
